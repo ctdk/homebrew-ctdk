fe32e35ef5650a9a991c48f72a3aee4ab8110600
ctdk <jbingham@gmail.com>
2016-08-31 11:53:16 -0700

    Initial commit; starting with a formula for bsdgames-osx

---
+++ README.md
@@ -0,0 +1,18 @@
+# Homebrew Grafana
+
+A homebrew tap for installing some programs I've written.
+
+## How do I use this?
+
+`brew tap ctdk/ctdk`
+
+Then, install the program you want with `brew install <program name>`.
+
+Read more at the [homebrew tap documentation](https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/brew-tap.md).
+
+## Contents of this tap
+
+* [`bsdgames-osx`](https://github.com/ctdk/bsdgames-osx)
+
+## Documentation
+`brew help`, `man brew` or check [Homebrew's documentation](https://github.com/Homebrew/brew/tree/master/share/doc/homebrew#readme).
---
+++ bsdgames-osx.rb
@@ -0,0 +1,33 @@
+# An update of the homebrew formula at
+# https://gist.github.com/bartdorsey/908002f74f1870b3135a for bsdgames-osx,
+# bringing it up to current homebrew standards. That formula in turn was an
+# updated version of https://gist.github.com/ctdk/5938940, to make it work on
+# Yosemite.
+
+class BsdgamesOsx < Formula
+  desc "The classic bsdgames of yore, for Mac OS X."
+  homepage "https://github.com/ctdk/bsdgames-osx"
+  url "https://github.com/ctdk/bsdgames-osx/archive/bsdgames-osx-2.19.3.tar.gz"
+  sha256 "699bb294f2c431b9729320f73c7fcd9dcf4226216c15137bb81f7da157bed2a9"
+  head "https://github.com/ctdk/bsdgames-osx.git"
+  depends_on "bsdmake" => :build
+
+  def install
+    ENV.j1
+    # This replicates the behavior of wargames calling games from /usr/games
+    inreplace "wargames/wargames.sh", "/usr/games", bin
+    system "CFLAGS=\"-std=c11\" bsdmake PREFIX=#{prefix} VARDIR=#{HOMEBREW_PREFIX}/var/games"
+    user = ENV["USER"]
+    ENV["BINOWN"] = user
+    ENV["LIBOWN"] = user
+    ENV["MANOWN"] = user
+    ENV["SHAREOWN"] = user
+    system "bsdmake", "install", "PREFIX=#{prefix}", "VARDIR=#{var}/games"
+  end
+
+  test do
+    %w[pom].each do |game|
+      system game
+    end
+  end
+end
