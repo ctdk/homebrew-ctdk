0b84a2ea48e1254de5b296f2121ad2d74e0e3ae3
ctdk <jbingham@gmail.com>
2019-05-15 08:13:33 -0700

    sticking this in a separate branch to test soon - xcode on this laptop is 'too out of date' apparently

--- bsdgames-osx.rb
+++ bsdgames-osx.rb
@@ -7,13 +7,15 @@
 class BsdgamesOsx < Formula
   desc "The classic bsdgames of yore, for Mac OS X."
   homepage "https://github.com/ctdk/bsdgames-osx"
-  url "https://github.com/ctdk/bsdgames-osx/archive/bsdgames-osx-2.19.3.tar.gz"
-  sha256 "699bb294f2c431b9729320f73c7fcd9dcf4226216c15137bb81f7da157bed2a9"
+  url "https://github.com/ctdk/bsdgames-osx/archive/bsdgames-osx-2.19.4.tar.gz"
+  sha256 "ae27651e709783e3f76038c85753d53ee36f86b4116802760c977595e284555d"
   head "https://github.com/ctdk/bsdgames-osx.git"
   depends_on "bsdmake" => :build
 
   def install
-    ENV.j1
+    # If, for whatever reason, ENV.deparallelize doesn't work for you, try
+    # changing it to ENV.j1
+    ENV.deparallelize
     # This replicates the behavior of wargames calling games from /usr/games
     inreplace "wargames/wargames.sh", "/usr/games", bin
     system "CFLAGS=\"-std=c11\" bsdmake PREFIX=#{prefix} VARDIR=#{HOMEBREW_PREFIX}/var/games"
